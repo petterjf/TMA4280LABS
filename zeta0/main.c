8b883328c666230b47e6436febd7ba8452cb657b
Petter Frisvåg <petterjf@stud.ntnu.no>
2017-02-21 16:35:54 +0100

    finished the zeta0 code

--- zeta0/main.c
+++ zeta0/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char **argv) {
+	if (argc != 2) return 1;
+
+	int n = atoi(argv[1]);
+	double sum = 0;
+	for (int i = 1; i <= n; i++) {
+		sum += (double) 1/(i*i);
+	}
+
+	double num_pi = sqrt(6*sum);
+	printf("%f\n", num_pi);
 
 	return 0;
 }
