d0bc1a38c53adcf8f2a05e3fb63c3f5597fc3b1a
Petter Frisvåg <petterjf@stud.ntnu.no>
2017-02-24 13:38:33 +0100

    added utest

---
+++ zeta0/utest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include "zeta.h"
+
+int main(int argc, char **argv) {
+	if (argc != 1) return 1;
+
+	double sum = zeta(3); 
+	double e_sum = 49.0/36.0; // expected sum
+
+	printf("%f\n", sum - e_sum);
+
+	return 0;
+}
