8acc0f2268d67a6cd5fe3b6c51c1f66bc8722c07
Petter Frisvåg <petterjf@stud.ntnu.no>
2017-02-27 11:12:25 +0100

    added a temp working logic for distributing the array

--- mach1/q5.c
+++ mach1/q5.c
@@ -13,37 +13,21 @@ int main(int argc, char **argv) {
 	MPI_Status status;
 	int tag = 100;
 
-	int root = size-1; // Make the last rank the root 
-
 	double log_size = log(size)/log(2);
 	if (floor(log_size) != ceil(log_size)) {
 		MPI_Abort(MPI_COMM_WORLD, MPI_ERR_DIMS);
 	}
 
 	int n_tot = atoi(argv[1]);
-	//if (n_tot < 2*size) {
-	//	MPI_Abort(MPI_COMM_WORLD, MPI_ERR_DIMS);
-	//}
 	int n = ceil((double) n_tot/size); // length of each vector
-	
-	// lower and upper limit to sum
-	int l = rank*n + 1;
-	int u = (rank+1)*n;
-	if (u > n_tot) {
-		if (l > n_tot) {
-			u = l;
-		} else {
-			u = n_tot;
-		}
+	int l = rank*n;
+	int u = (rank+1)*n - 1;
+	if (l >= n_tot) {
+		l = u = 0;
+	} else if (u >= n_tot) {
+		u = n_tot - 1;
 	}
-
 	printf("I am %d, with (%d,%d)\n", rank, l, u);
-	double sum = 0;
-
-	MPI_Barrier(MPI_COMM_WORLD);
-	/*if (rank == root) {
-		printf("%d, %d\n", n, n_tot);
-	}*/
 
 	MPI_Finalize();
 	return 0;
